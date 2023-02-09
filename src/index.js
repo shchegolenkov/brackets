1cd9c65bdf6b14ca96b7871285eb8bb37152bf04
shchegolenkov <denis.amorales@ya.ru>
2023-02-09 22:16:02 +0300

    feat: brackets check done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const openBrackets = [];
+  const closeBrackets = [];
+  const pairBrackets = {};
+  const stack = [];
+
+  bracketsConfig.forEach(function (array) {
+    openBrackets.push(array[0]);
+    closeBrackets.push(array[1]);
+    pairBrackets[array[1]] = array[0];
+  });
+
+  for (let i = 0; i < str.length; i++) {
+    if (openBrackets.includes(str[i]) && closeBrackets.includes(str[i])) {
+      if (stack.length > 0 && stack[stack.length - 1] == pairBrackets[str[i]]) {
+        stack.pop();
+      } else {
+        stack.push(str[i]);
+      }
+    } else if (openBrackets.includes(str[i])) {
+      stack.push(str[i]);
+    } else {
+      if (!stack.length) {
+        return false;
+      }
+      if (stack[stack.length - 1] == pairBrackets[str[i]]) {
+        stack.pop();
+      }
+    }
+  }
+  return stack == 0;
+};
